9141b71cc7049b6520d5141b2d7016881931af93
Vito <vitohartanto@mail.ugm.ac.id>
2024-10-25 13:24:15 +0700

    fix redisClient

--- cache/redisClient.js
+++ cache/redisClient.js
@@ -1,16 +1,13 @@
 const redis = require('redis');
 
 const redisClient = redis.createClient({
-  host: process.env.REDIS_HOSTNAME,
-  port: process.env.REDIS_PORT,
+  url: `redis://${process.env.REDIS_HOSTNAME}:${process.env.REDIS_PORT}`,
   password: process.env.REDIS_PASSWORD,
 });
 
-redisClient.connect();
-
 redisClient.on('connect', () => {
   console.log('Connected to our redis instance!');
-  redisClient.set('Greatest Basketball Player', 'Lebron James');
+  redisClient.set('basketball', 'Lebron James');
 });
 
 redisClient.on('error', (err) => console.error('Redis Client Error', err));
